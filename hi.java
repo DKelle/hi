a592b865908c853a1323238e7c7f1a288567a34d
Dallas Kelle <dallas.kelle@gmail.com>
2017-11-08 08:55:57 -0600

    now saying hi to the class

--- hi.java
+++ hi.java
@@ -3,7 +3,7 @@ public class hi
 
     public static void main(String[] args)
     {
-        System.out.println("hi");
-        System.out.println("bye");
+        System.out.println("hi class");
+        System.out.println("bye class");
     }
 }
